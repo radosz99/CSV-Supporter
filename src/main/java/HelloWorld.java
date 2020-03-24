a3711f1402797cbe4819688809b107c429878f25
radosz99 <47248979+radosz99@users.noreply.github.com>
2020-03-24 23:41:13 +0100

    Add another method to show string

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,4 +2,7 @@ public class HelloWorld {
     public void showHelloWorld(){
         System.out.println("HelloWorld");
     }
+    public void showSomethingElse(String message){
+        System.out.println(message);
+    }
 }
