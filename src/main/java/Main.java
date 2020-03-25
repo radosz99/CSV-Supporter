7249ec44a82d9896e7d3b1bd5d2976fe9bcb0ac8
radosz99 <47248979+radosz99@users.noreply.github.com>
2020-03-25 15:09:18 +0100

    Use methods from biblioteka branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[]args){
-      HelloWorld hello = new HelloWorld();
-      hello.showHelloWorld();
+    public static void main(String[]args) throws IOException {
+        File csvFile = new File("C:\\Users\\Radek\\Desktop\\6semestr\\DPPTes\\file.csv");
+        ArrayList<ArrayList<String>> data = CSVHandler.parseCSV(csvFile);
+        ArrayList<Integer> columns = new ArrayList<>();
+        columns.add(0);
+        columns.add(2);
+        ArrayList<ArrayList<String>> data2 = CSVHandler.getByMultipleRow(columns,data);
+        ArrayList<String> tescik = CSVHandler.getByRow(3,data);
+        for(String s :tescik)
+            System.out.println(s);
+        CSVHandler csvHandler = new CSVHandler();
+        csvHandler.saveToCSV(data2, ";", "test.csv");
+        //CSVHandler.showCSV(data2);
     }
 }
