d2c23a94eed9fdc387432e3cb902bdc2f6c6d5ab
radosz99 <47248979+radosz99@users.noreply.github.com>
2020-03-25 15:00:00 +0100

    Add methods for get multiple rows and columns

--- src/main/java/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public void showHelloWorld(){
-        System.out.println("HelloWorld");
-    }
-    public void showSomethingElse(String message){
-        System.out.println(message);
-    }
-}
